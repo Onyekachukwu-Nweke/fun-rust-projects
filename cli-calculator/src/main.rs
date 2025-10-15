630ba436340f73cd9b7c490a1b9399022f7a3cf9
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-10-15 14:15:42 +0100

    feat: cli calculator implementation

--- cli-calculator/src/main.rs
+++ cli-calculator/src/main.rs
@@ -1,3 +1,38 @@
+mod ops;
+
+use clap::Parser;
+use ops::{add, sub, mul, div};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// operand
+    op: String,
+
+    /// argument 1
+    arg1: f64,
+
+    /// argument 1
+    arg2: f64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let op = args.op;
+    let lhs = args.arg1;
+    let rhs = args.arg2;
+
+    let result = match op.as_str() {
+        "add" | "+" => add(lhs, rhs),
+        "sub" | "-" => sub(lhs, rhs),
+        "mul" | "*" => mul(lhs, rhs),
+        "div" | "/" => div(lhs, rhs),
+        _ => {
+            println!("Unsupported operation!");
+            return;
+        }
+    };
+
+    println!("Result: {}", result);
 }
---
+++ cli-calculator/src/ops.rs
@@ -0,0 +1,15 @@
+pub(crate) fn add(a: f64, b: f64) -> f64 {
+    return a + b;
+}
+
+pub(crate) fn sub(a: f64, b: f64) -> f64 {
+    return a - b;
+}
+
+pub(crate) fn mul(a: f64, b: f64) -> f64 {
+    return a * b;
+}
+
+pub(crate) fn div(a: f64, b: f64) -> f64 {
+    return a / b;
+}
