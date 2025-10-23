8b86022281b78f335e34de3f0c859f920bfddfe2
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-10-23 10:58:35 +0100

    feat: task struct implementation

---
+++ tasker/src/task.rs
@@ -0,0 +1,32 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Task {
+    pub id: Uuid,
+    pub status: Status,
+    pub title: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl Task {
+    pub fn new(title: String) -> Self {
+        let now = Utc::now();
+        Self {
+            id: Uuid::new_v4(),
+            status: Status::Todo,
+            title,
+            created_at: now,
+            updated_at: now,
+        }
+    }
+}
